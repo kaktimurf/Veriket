dc79ad5cb713506d8228aa932d7577cfc5939b4e
Ferit Kaktimur <ferit.kaktimur@devbase.com.tr>
2024-02-05 16:39:56 +0300

    add setup project

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Kurulum dosyaları Veriket.Setup/Debug dizininde mevcuttur.
+Not
+Windows service için bir worker projesi kullandım bu worker projesi bir nuget paketi ile 
--- Veriket.Worker/Program.cs
+++ Veriket.Worker/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Veriket.Worker;
 
 IHost host = Host.CreateDefaultBuilder(args)
@@ -15,4 +16,47 @@ IHost host = Host.CreateDefaultBuilder(args)
     })
     .Build();
 
+/*
+ RunExistingBat metodu worker projesini derlendikten sonra oluşan bir bat dosyasını bir defaya mahsus çalıştırarak 
+ bu projeyi bir windows servisi olarak ayağa kaldırmaktadır.
+ Bat dosyasının içeriği:
+    sc create "Veriket Test App" binPath="..." start=auto
+    sc start "Veriket Test App"
+ */
+RunExistingBat(); 
 host.Run();
+
+static void RunExistingBat()
+{
+    string ConfigFileName = "RunFlag.txt";
+    string configFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+    if (File.Exists(configFilePath))
+    {
+        Console.WriteLine("Service zaten oluşturulmuş.");
+    }
+    else
+    {
+        File.Create(configFilePath);
+
+
+        string batFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "InstallService.bat");
+        if (File.Exists(batFilePath))
+        {
+            // Oluşturduğum bat dosyasını çalıştır bi nevi cmd 
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = batFilePath,
+                UseShellExecute = true,
+                CreateNoWindow = true,
+                Verb = "runas" // Yönetici olarak çalıştır
+            };
+
+            Process.Start(psi);
+        }
+        else
+        {
+            Console.WriteLine("Bat dosyası bulunamadı.");
+        }
+    }
+
+}
