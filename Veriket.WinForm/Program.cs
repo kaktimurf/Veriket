0b0ad367b27aa3a4e8ef49d27214ef4ec4b97ea8
Ferit Kaktimur <ferit.kaktimur@devbase.com.tr>
2024-02-05 15:45:58 +0300

    fixed

--- Veriket.WinForm/Program.cs
+++ Veriket.WinForm/Program.cs
@@ -16,44 +16,7 @@ namespace Veriket.WinForm
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            RunExistingBat();
             Application.Run(new Form1());
-        }
-
-        static void RunExistingBat()
-        {
-            string ConfigFileName = "RunFlag.txt";
-            string configFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
-            if (File.Exists(configFilePath))
-            { 
-
-            }
-            else
-            {
-                File.Create(configFilePath);
-                var workerProjectPathRelative = AppDomain.CurrentDomain.BaseDirectory.Split("\\Veriket.WinForm\\");
-                string workerProjectPath = Path.GetFullPath(Path.Combine(workerProjectPathRelative[0], "Veriket.Worker\\bin\\Debug\\net7.0"));
-
-                string batFilePath = Path.Combine(workerProjectPath,"InstallService.bat");
-                if (File.Exists(batFilePath))
-                {
-                    // Var olan bat dosyasını çalıştır
-                    ProcessStartInfo psi = new ProcessStartInfo
-                    {
-                        FileName = batFilePath,
-                        UseShellExecute = true,
-                        CreateNoWindow = true,
-                        Verb = "runas" // Run as administrator
-                    };
-
-                    Process.Start(psi);
-                }
-                else
-                {
-                    Console.WriteLine("Var olan bat dosyası bulunamadı.");
-                }
-            }
-            
         }
     }
 }
